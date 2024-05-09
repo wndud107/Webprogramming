f689df161e655e4f1032d7cdae032c5a1b4e56aa
wndud107 <127063315+wndud107@users.noreply.github.com>
2024-05-09 11:06:44 +0900

    Add files via upload

---
+++ 실습/ch5/condition.js
@@ -0,0 +1,36 @@
+/*
+if (true) {
+    console.log('무조건 참입니다.');
+} 
+
+var num = 20;
+if (num > 10){
+    console.log('입력값은 10보다 큽니다.');
+}
+if (num < 10){
+    console.log('입력값은 10보다 작습니다.');
+}
+if (num == 10){
+    console.log('입력값은 10과 같습니다.');
+} 
+
+var num = 5;
+if (num > 10){
+    console.log('입력값은 10보다 큽니다.');
+}
+else{
+    console.log('입력값은 10보다 크지 않습니다.');
+} */
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input : process.stdin, output : process.stdout,
+})
+
+rl.question('프로그래밍 언어 이름을 입력하시오. : ', function(data){
+    //입력값에 대한 처리
+    console.log('가장 좋아하는 프로그래밍 언어는 ' +data + '입니다.');
+    rl.close();
+})
